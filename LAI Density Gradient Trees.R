9bfd622e1a33b43c5a29f5217bb07eb9af3ecb80
sscardarelli <scardarelli@colgate.edu>
2019-06-11 12:00:45 -0400

    wrote code for calculating foliar biomass depending on density & calculating leaf area

--- LAI Density Gradient Trees.R
+++ LAI Density Gradient Trees.R
@@ -5,16 +5,16 @@ setwed("C:/Users/scardarelli/Documents/research/siberia_lai")
 
 library(dplyr)
 
-#read in data only to row 5012 because plot value is incorrect
+#read in data only to row 5011 because plot value is incorrect
 tree_data<-read.csv("2010 - 2017 Density Gradient Trees 3_13_19.csv",
-                    nrows=5011)[,c(2:6,12)]
+                    nrows=5011)[,c(2:7,12)]
 
 #things I've done so far:
 f_bio<-tree_data$Foliage.biomass..g.dry.wt.
 
 hbr_1<-tree_data[1:55,1:2]
 
-hbr_1_fb<-tree_data[1:55,c(1:2,6)]
+hbr_1_fb<-tree_data[1:55,c(1:2,7)]
 sum(hbr_1_fb$Foliage.biomass..g.dry.wt.)
 
 #so from here I got the sum of just one site of one plot. this would take a loooonnnggg time
@@ -31,7 +31,7 @@ sum(hbr_1_fb$Foliage.biomass..g.dry.wt.)
 #should equal 14621.75
 #first value on the foliar_biomass is also 14621.75!
 
-hdf1_1_fb<-tree_data[944:991,c(1:2,6)]
+hdf1_1_fb<-tree_data[944:991,c(1:2,7)]
 sum(hdf1_1_fb$Foliage.biomass..g.dry.wt.)
 #should equal 21251.68, just like foliar_biomass!
 
@@ -41,3 +41,43 @@ foliar_biomass
 
 #reorganize by Site name, not Plot
 fol_bio<-arrange(foliar_biomass, foliar_biomass$Site)
+
+#dplyr
+tree_data %>%
+  group_by(Site, Plot) %>%
+  summarise(total_biomass = sum(Foliage.biomass..g.dry.wt.)) %>%
+  arrange(Site)
+
+#creating another column for density: "H" or "L"
+#we need to use different allometry for low density and high & medium density sites
+#this column will allow us to differentiate between the two
+tree_data$dens <- ifelse(substr(tree_data$Site,1,1) == "L",
+                          "L","H")
+
+#recalculating foliar biomass with new allometry
+by_dens <- arrange(tree_data,tree_data$dens)
+
+#for now, split into high density and low density charts
+#(until I can find a better way)
+high_dens<-by_dens[1:4217,]
+low_dens<-by_dens[4218:5011,]
+
+high_diameter<-high_dens$Diam..cm.
+low_diameter<-low_dens$Diam..cm.
+
+#and calculate new biomass on both, adding to a new column
+high_dens$total_foliar<-7.57*high_diameter^1.73
+low_dens$total_foliar<-150.5*low_diameter
+
+#calculate leaf area using SLA for high and low density
+#this is currently using SLA with cm/g as units cause diameter is in cm and foliar in g
+high_dens$leaf_area<-high_dens$total_foliar*112.89
+low_dens$leaf_area<-low_dens$total_foliar*84.69
+#these are very high numbers and I've never done this before so idk if they're right
+
+#all my tables are messy/full of columns so I'm just going to condense quickly
+#you may not need to do this
+low_density<-low_dens[c(1:3,6,8,9,11)]
+high_density<-high_dens[c(1:3,6,8,9,11)]
+
+#renaming columns
