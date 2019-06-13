92e9bbda8277bf2be425dd4b901d3fade9b72481
sscardarelli <scardarelli@colgate.edu>
2019-06-13 10:01:28 -0400

    corrected error in lai calculation. took out some early notes/lines of code to neaten and clarify

--- LAI Density Gradient Trees.R
+++ LAI Density Gradient Trees.R
@@ -9,30 +9,11 @@ library(dplyr)
 tree_data<-read.csv("2010 - 2017 Density Gradient Trees 3_13_19.csv",
                     nrows=5011)[,c(2:7,12)]
 
-#things I've done so far:
-f_bio<-tree_data$Foliage.biomass..g.dry.wt.
-
-hbr_1<-tree_data[1:55,1:2]
-
-hbr_1_fb<-tree_data[1:55,c(1:2,7)]
-sum(hbr_1_fb$Foliage.biomass..g.dry.wt.)
-
 #so from here I got the sum of just one site of one plot. this would take a loooonnnggg time
 #to do one at a time for each so. gotta figure out a quicker way.
 
 foliar_biomass<-aggregate(tree_data$Foliage.biomass..g.dry.wt., 
                           by=list(tree_data$Site, tree_data$Plot), FUN=sum)
-
-#and we can double check if they're correct by checking a few of the smaller ones
-#like how i made earlier. for example:
-
-sum(hbr_1_fb$Foliage.biomass..g.dry.wt.)
-#should equal 14621.75
-#first value on the foliar_biomass is also 14621.75!
-
-hdf1_1_fb<-tree_data[944:991,c(1:2,7)]
-sum(hdf1_1_fb$Foliage.biomass..g.dry.wt.)
-#should equal 21251.68, just like foliar_biomass!
 
 #rename columns
 colnames(foliar_biomass)<-c("Site","Plot","Sum of Foliage Biomass")
@@ -70,8 +51,8 @@ low_dens$total_foliar<-150.5*low_diameter
 
 #calculate leaf area using SLA for high and low density
 #the SLA i was given was in cm^2 so i converted to m^2 for this
-high_dens$leaf_area<-high_dens$total_foliar*1.1289
-low_dens$leaf_area<-low_dens$total_foliar*0.8469
+high_dens$leaf_area<-high_dens$total_foliar*(112.89/10000)
+low_dens$leaf_area<-low_dens$total_foliar*(84.69/10000)
 
 #all my tables are messy/full of columns so I'm just going to condense quickly
 #you may not need to do this
@@ -88,13 +69,16 @@ colnames(low_density)<-c("Site", "Plot", "Sample Area", "Diameter (cm)",
 #thought might make it easier on me moving forward
 lai_data<-full_join(high_density,low_density)
 
-#now calculate LAI and add a column to the table for this
-lai_data$LAI<-lai_data$`Leaf Area (m^2)`/lai_data$`Sample Area`
+#6/13/19 edit: adding up leaf area per site/plot
+leaf_area<-aggregate(lai_data$`Leaf Area (m^2)`, 
+                           by=list(lai_data$Site, lai_data$Plot, lai_data$`Sample Area`), FUN=sum)
+colnames(leaf_area)<-c("Site", "Plot", "Plot Area", "Total Leaf Area")
+leaf_area<-arrange(leaf_area, leaf_area$Site)
 
-#condense by site and plot
-tree_lai_data<-aggregate(lai_data$LAI, by=list(lai_data$Site, lai_data$Plot), FUN=sum)
-colnames(tree_lai_data)<-c("Site", "Plot", "LAI")
-tree_lai_data<-arrange(tree_lai_data, tree_lai_data$Site)
+#when get a chance: practice making this same table with dplyr
+
+#now calculate LAI and add a column to the table for this
+leaf_area$LAI<-leaf_area$`Total Leaf Area`/leaf_area$`Plot Area`
 
 #practice making it with dplyr
 tree_lai_data1<-lai_data %>%
